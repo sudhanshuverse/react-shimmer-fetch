c6c86172a8dafc7f3a1bafddb2c70008a368be31
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-11-13 19:00:47 +0530

    Configured Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Set correct base path for GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  base: '/react-shimmer-fetch/',
 })
